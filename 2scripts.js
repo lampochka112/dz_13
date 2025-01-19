a5bc1c35091d1f60edaada50a456cfa7ff2c52f6
LEV3457 <lukyanovivan83@gmail.com>
2025-01-19 20:10:28 +0300

    lol

---
+++ 2scripts.js
@@ -0,0 +1,8 @@
+document.getElementById('button').addEventListener('click', function(){
+    if (confirm('ты правдадашь мне 1 млн робуксов?')){
+        alerty(' Ура я теперь богат!!!!')
+    }else{
+        alert('жди ддос атаку говнюк!!')
+    }
+
+}); 
